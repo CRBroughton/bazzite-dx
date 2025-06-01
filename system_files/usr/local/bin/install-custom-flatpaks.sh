b1e99d17b3677da41507247858f9142eb61d7a59
CRBroughton <crbroughton@posteo.uk>
2025-06-01 15:46:49 +0100

    add a few apps

--- system_files/usr/local/bin/install-custom-flatpaks.sh
+++ system_files/usr/local/bin/install-custom-flatpaks.sh
@@ -8,12 +8,14 @@ flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/fla
 
 # Install Flatpaks directly (no external file needed)
 flatpak install --system --noninteractive --assumeyes flathub \
-    com.discordapp.Discord \
     md.obsidian.Obsidian \
     com.bitwarden.desktop \
     org.gnome.World.PikaBackup/x86_64/stable \
     org.qbittorrent.qBittorrent \
-    com.spotify.Client
+    com.spotify.Client \
+    io.github.equicord.equibop/x86_64/stable \
+    app.zen_browser.zen/x86_64/stable \
+    com.yubico.yubiath/x86_64/stable
 
 # Create marker file so it only runs once
 touch /var/lib/custom-flatpaks-installed

c1c321f3bc5f1fe1e7db4801be1db183c1a94af0
CRBroughton <crbroughton@posteo.uk>
2025-06-01 13:53:39 +0100

    chmod

--- build_files/55-enable-services.sh
+++ build_files/55-enable-services.sh
@@ -3,9 +3,6 @@ set -euo pipefail
 
 echo "Enabling custom flatpaks service..."
 
-# Make the script executable
-chmod +x /usr/local/bin/install-custom-flatpaks.sh
-
 # Enable the service globally for all users
 systemctl --global enable custom-flatpaks.service
 
